2fd7c8d6aea23a6a444ef758a629fb66437a9a4e
AnAmusingGhost <cflore63@depaul.edu>
2025-01-13 15:24:44 -0600

    Added question 2 solution on final exam folder

---
+++ Final Exam/question2.py
@@ -0,0 +1,10 @@
+roman_numerals = {'I': 1, 'II': 2, 'III': 3, 'V': 5, 'X': 10, 'L': 50} 
+
+keys = list(roman_numerals.keys())
+print("The keys are: ", keys)
+
+values = list(roman_numerals.values())
+print("the values are: ", values)
+
+items = list(roman_numerals.items())
+print("The items are: ", items)
